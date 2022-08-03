1c93fb00f5e3c520c9d6472e0fbca5c0f8dfc0b2
0x6Star3s <95292019+0x6Star3s@users.noreply.github.com>
2022-08-03 08:47:31 +0100

    Update block-number.js

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -1,7 +1,6 @@
 const {task} = require("hardhat/config")
 
 task("block-number", "prints the current block number").setAction(
-    // to działa jak async funkcjią name 
     async (taskArgs, hre) => {
         const blockNumber = await hre.ethers.provider.getBlockNumber()
         console.log(`Current block number: ${blockNumber}`)
@@ -9,8 +8,4 @@ task("block-number", "prints the current block number").setAction(
     }
 )
 
-// module który exportuje całą funkcje 
 module.exports = {}
-
-// domyślnie hardhat testuje na swoim lockalnym sieci i liczba bloków jest 0 
-// jeżeli testujemy rinkeby(yarn hardhat block-number --network rinkeby) to bedziemy testwać na rinkeby i liczba blocków bedzie 11113744
